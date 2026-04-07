df56f35894541262d4bfe5835ef8bb58c2d0a3ed
Gutturu Mokshit Balaji <gmokshitbalaji@gmail.com>
2026-04-07 14:09:38 +0530

    QR generator

---
+++ qrgenerator.py
@@ -0,0 +1,8 @@
+import qrcode
+text_message = "HI!, I JUST STARTED DOING MINI PROJECTS..."
+qr = qrcode.QRCode()
+qr.add_data(text_message)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("QR.png")
+print("Succes , saved as image")
