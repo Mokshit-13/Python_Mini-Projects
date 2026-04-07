20eda7e099746274a93657ebf65a3b533922394c
Gutturu Mokshit Balaji <gmokshitbalaji@gmail.com>
2026-04-07 15:01:05 +0530

    Success

--- qrgenerator.py
+++ qrgenerator.py
@@ -1,8 +1,11 @@
 import qrcode
+
 text_message = "HI!, I JUST STARTED DOING MINI PROJECTS..."
 qr = qrcode.QRCode()
 qr.add_data(text_message)
 qr.make(fit=True)
+
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("QR.png")
-print("Succes , saved as image")
+
+print("Success, saved as image")
